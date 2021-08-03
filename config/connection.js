4bdcce040fcb994af499fb8bff7c63833541ac21
macoovacany <macoovacany@hotmail.com>
2021-08-03 21:25:33 +0930

    Initial Commit

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
+require('dotenv').config();
+
+console.log(process.env.MONGODB_URI )
+
+mongoose.connect(process.env.MONGODB_URI , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
