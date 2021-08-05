c9a070269233fe80adc76ab772e784cacbd659b7
macoovacany <macoovacany@hotmail.com>
2021-08-05 19:14:18 +0930

    interim changes to force rebuild

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 
+const cors = require('cors');
+
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
@@ -16,6 +18,7 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
